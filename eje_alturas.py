5a2696b9e83ce66903db0eeee0cc41a8626fc22d
JohanC664 <johan.a.cardona.l.2006@gmail.com>
2024-02-06 20:05:42 -0500

    ejercicio uno

---
+++ eje_alturas.py
@@ -0,0 +1,15 @@
+
+estatura = 1
+suma  = 0
+cont = 0
+while estatura > 0:
+    estatura = float (input("si desea dejar e digitar estaturas digite 0 \n" "por favor ingrese una esatura: " ))
+        
+    if estatura > 0:
+        suma += estatura
+        cont += 1
+        
+if cont > 0:
+    print (f"el promedio de las esataturas es {suma/cont}")
+        
+        
